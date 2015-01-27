d4d4acfe112e20a3645cf3b29e3b7585fb5e2336
unknown <papin3049>
2015-01-27 16:55:25 +0600

    signal r chat app try

---
+++ SignalRChatAngularWebApplication/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChatAngularWebApplication
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string username, string message)
+        {
+            Clients.All.broadcastMessage(username,message);
+        }
+    }
+}
---
+++ SignalRChatAngularWebApplication/app/scripts/Controllers/ChatController.js
@@ -0,0 +1,25 @@
+﻿
+
+app.controller('chatController', ['$scope', function ($scope) {
+
+    $scope.messages = [];
+    var chat = $.connection.chatHub;
+
+    chat.client.broadcastMessage = function (name, message) {
+
+        $scope.messages.push({ Username: name, Message: message });
+        $scope.$apply();
+    };
+    
+    $.connection.hub.start().done(function () {
+
+
+    });
+
+    $scope.send = function () {
+
+        chat.server.send($scope.username, $scope.message);
+
+    };
+
+}]);
